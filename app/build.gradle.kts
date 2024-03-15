0243180d65a2c8b6c2539fc6cabed8d7fa32c6c1
Enrique Izel <eleaoizel@gmail.com>
2024-03-15 10:45:47 -0400

    feat: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,10 @@ dependencies {
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+  //Retrofit
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+  //ViewModel e Livedata - Dependências para MVVM
+  implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 }
